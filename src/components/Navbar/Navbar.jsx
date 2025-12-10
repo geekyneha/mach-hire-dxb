f26e0308ec55461eb9a018c4967fc829189ecc2e
geekyneha <nehatualea@gmail.com>
2025-12-10 21:47:35 +0530

    feat: add responsive mobile navbar with toggle button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,45 +1,54 @@
 // Navbar.jsx
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
+import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>ExcavatorRent</div>
-      <ul className={styles.navLinks}>
+
+      {/* Menu Toggle Button */}
+      <button className={styles.menuBtn} onClick={() => setOpen(!open)}>
+        {open ? <FaTimes size={22} /> : <FaBars size={22} />}
+      </button>
+
+      <ul className={`${styles.navLinks} ${open ? styles.open : ""}`}>
         <li>
-          <Link to="/" className={styles.links}>
+          <Link to="/" onClick={() => setOpen(false)}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="/about" className={styles.links}>
+          <Link to="/about" onClick={() => setOpen(false)}>
             About Us
           </Link>
         </li>
         <li>
-          <Link to="/services" className={styles.links}>
+          <Link to="/services" onClick={() => setOpen(false)}>
             Services
           </Link>
         </li>
         <li>
-          <Link to="/contact" className={styles.links}>
+          <Link to="/contact" onClick={() => setOpen(false)}>
             Contact
           </Link>
         </li>
         <li>
-          <Link to="/booking" className={styles.links}>
+          <Link to="/booking" onClick={() => setOpen(false)}>
             Book Now
           </Link>
         </li>
         <li>
-          <Link to="/about" className={styles.links}>
+          <Link to="/about" onClick={() => setOpen(false)}>
             More Info
           </Link>
         </li>
         <li>
-          <Link to="/faq" className={styles.links}>
+          <Link to="/faq" onClick={() => setOpen(false)}>
             FAQ
           </Link>
         </li>
