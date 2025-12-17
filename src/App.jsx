b127ae09ce2a8a6932eb0162495f8f94904c1841
geekyneha <nehatualea@gmail.com>
2025-12-17 20:11:33 +0530

    chore: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import FAQ from "./pages/FAQ.jsx";
 import Excavators from "./components/Equipments/Excavators.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Equipments from "./pages/Equipments.jsx";
+import CategoryPage from "./components/Category/CategoryPage.jsx";
+import Careers from "./pages/Careers.jsx";
+import WheelLoaders from "./components/Equipments/WheelLoaders.jsx";
+import DumpTrucks from "./components/Equipments/DumpTrucks.jsx";
+import SixWheelers from "./components/Equipments/SixWheelers.jsx";
 
 function App() {
   const router = createBrowserRouter([
@@ -22,10 +27,21 @@ function App() {
         { path: "/services", element: <Services /> },
         { path: "/booking", element: <Booking /> },
         { path: "/faq", element: <FAQ /> },
-        { path: "/excavators", element: <Excavators /> },
+        { path: "/careers", element: <Careers /> },
+        { path: "/equipment/excavators", element: <Excavators /> },
+        { path: "/equipment/wheel-loaders", element: <WheelLoaders /> },
+        { path: "/equipment/dump-trucks", element: <DumpTrucks /> },
+        {
+          path: "/equipment/six-wheelers",
+          element: <SixWheelers />,
+        },
         {
           path: "/equipments",
           element: <Equipments />,
+        },
+        {
+          path: "/equipments/:category",
+          element: <CategoryPage />,
         },
       ],
     },
