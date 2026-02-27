646ad5131a1af23141791b496fa8e56e71eea219
geekyneha <nehatualea@gmail.com>
2026-02-27 21:34:40 +0530

    SEO

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -7,7 +7,7 @@ const SEO = ({ title, description, canonical }) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       {canonical && <link rel="canonical" href={canonical} />}
-      {/* Open  */}
+      {/* Open Graph */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
